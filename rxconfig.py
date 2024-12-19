17d67165cbbfec91df620bb277fbb6a4a557c478
Chance Callahan <chance@chancecallahan.com>
2024-12-19 00:57:59 -0500

    Making another fix for Railway.

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,7 @@
 import reflex as rx
+import os
 
 config = rx.Config(
     app_name="rolemgr",
+    api_url=f'https://{os.environ[railway_domain]}/backend' if railway_domain in os.environ else "http://127.0.0.1:8000"
 )
